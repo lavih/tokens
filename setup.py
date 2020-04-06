e252d4029f97f9dbaa9a40228da5bc2b6048324e
lavih <47455309+lavih@users.noreply.github.com>
2020-04-06 18:06:26 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pnp',
-    version='0.0.1',
+    version='0.0.2',
     description='My private package from private github repo',
     url='https://github.com/lavih/tokens.git',
     author='lavih',
