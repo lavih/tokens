218c51c11a58e353b5a0eeaa34b7efb6012e4213
lavih <lavih44@walla.co.il>
2020-04-06 11:12:15 +0300

    api_fe

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pnp',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='https://github.com/lavih/tokens.git',
+    author='lavih',
+    author_email='lavih@walla.co.il',
+    license='unlicense',
+    packages=['pnp'],
+    zip_safe=False
+)
